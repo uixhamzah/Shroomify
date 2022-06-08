2e54d6d76c82ecb45049eabdb23aff6f756e066a
Ilham Naufal <ngim868@gmail.com>
2022-06-08 13:35:45 +0700

    adding dataset and running script

---
+++ Machine Learning/import_balancing_dataset.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+from functions import preprocess
+# from functions import balance
+
+base_path = "data/edible-and-poisonous-fungi/data"
+train_df, test_df, valid_df= preprocess(base_path, .8,.1)
+
+# max_samples= 688
+# min_samples=0
+# column='labels'
+# working_dir = r'./'
+# img_size=(300, 500)
+# ndf=balance(train_df,max_samples, min_samples, column, working_dir, img_size)
